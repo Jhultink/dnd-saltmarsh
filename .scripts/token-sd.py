438f5242bc361618c51f6b525d380dc06eba3cb0
Saltmarsh DM <jdhultink@gmail.com>
2026-05-01 10:07:49 -0400

    no prompt

--- .scripts/token-sd.py
+++ .scripts/token-sd.py
@@ -5,7 +5,7 @@ pipe = AutoPipelineForText2Image.from_pretrained('lykon/dreamshaper-xl-v2-turbo'
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to("cuda")
 
-prompt = "portrait photo bearded bard with a lute, fantasy RPG portrait, circular vignette, painterly illustration style, detailed face, neutral background"
+prompt =""
 
 generator = torch.manual_seed(0)
 image = pipe(prompt, num_inference_steps=6, guidance_scale=2).images[0]  
